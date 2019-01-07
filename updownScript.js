ca24324a120c29fc138868abb3cc2ab55636c304
killofki <killofki@yahoo.co.kr>
2019-01-07 19:07:31 +0900

    \n's space invader

--- updownScript.js
+++ updownScript.js
@@ -1,13 +1,12 @@
 { 
 /// 
- 
+
 let answer; 
 let guess; 
 let turn = 0; 
 let gameStart = false; 
 let postposition; 
- 
-const d = document; 
+
 const queryBtn = $( '#query' ); 
 const msg = $( '#msg' ); 
 const atc = $( 'article'); 
@@ -15,48 +14,48 @@ const inpt = $( 'input' );
 const msgAbove = $( '#message-above' ); 
 const rst = $( '#result' ); 
 const msgBelow = $( '#message-below' ); 
- 
+
 function $( q ) { return document .querySelector( q ); } 
- 
-let initGameEventHandler = queryBtn.addEventListener("click", btnHandler); 
- 
+
+let initGameEventHandler = queryBtn. addEventListener( "click", btnHandler ); 
+
 function fadeIn(sth) { 
     sth.classList.add("active"); 
     setTimeout(() => {sth.classList.add("show")	}, 20); 
 	} 
- 
+
 function fadeOut(sth) { 
     sth.classList.remove("show"); 
     setTimeout(() => {sth.classList.remove("active")	}, 200); 
 	} 
- 
+
 function btnHandler() { 
     if (gameStart) { 
         guess = parseInt(inpt.value); 
- 
+
         if ( isNaN(guess) || guess < 1 || guess > 100 ) { 
             msg.innerHTML = "1부터 100까지의 숫자 중 하나를 입력해주세요!"; 
         	} else { 
             inpt.value = ""; 
             answering(guess); 
         	} 
- 
+
     	} else { 
         initializeGame(); 
     	} 
 	} 
- 
+
 function initializeGame() { 
     fadeOut(atc); 
     turn = 0; 
     queryBtn.classList.add("deactivated"); 
     queryBtn.innerHTML = "기다리세요!"; 
     msg.innerHTML = "1부터 100까지의 숫자 중 하나를 생각하는 중입니다…"; 
- 
+
     answer = Math.floor(Math.random() * 100) + 1; 
     setTimeout(startGame, 1000); 
 	} 
- 
+
 function startGame() { 
     queryBtn.classList.remove("deactivated"); 
     queryBtn.innerHTML = "추측하기"; 
@@ -64,18 +63,18 @@ function startGame() {
     msgAbove.innerHTML = "1번째 시도입니다."; 
     msgBelow.innerHTML = ""; 
     rst.innerHTML = "？"; 
- 
+
     fadeIn(atc); 
     fadeIn(inpt); 
     gameStart = true; 
- 
+
     inpt.onkeydown = function(e) { 
         if (e.keyCode === 13) { 
             btnHandler(); 
         	} 
     	}; 
 	} 
- 
+
 function answering(num) { 
     if (num === "") { 
         return; 
@@ -89,18 +88,18 @@ function answering(num) {
         	} 
     	} 
 	} 
- 
+
 function turnPass() { 
     turn++; 
     msgAbove.innerHTML = turn.toString() + "번째 시도입니다."; 
 	} 
- 
+
 function result(judge, num) { 
     if (judge === true) { 
         rst.className = ""; 
         rst.innerHTML = answer; 
         msgBelow.innerHTML = "정답은 " + answer + "입니다!"; 
- 
+
         gameStart = false; 
         queryBtn.innerHTML = "다시 하기"; 
         msg.innerHTML = "게임을 다시 시작하려면 위의 버튼을 눌러주세요."; 
@@ -112,7 +111,7 @@ function result(judge, num) {
         msgBelow.innerHTML = guess + choosePostposition(num) + " 아닙니다!" 
     	} 
 	} 
- 
+
 function choosePostposition(number) { 
     noun = parseInt(number) % 10; 
     if (noun === 2 || noun === 4 || noun === 5 || noun === 9) { 
@@ -121,7 +120,7 @@ function choosePostposition(number) {
         return "이"; 
     	} 
 	} 
- 
+
 // src from https://doro-nyong.github.io/up-and-down-js/ 
 /// 
 } 
