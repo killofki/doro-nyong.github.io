7b2f448b007996d5b231ae947844a68e1628b604
killofki <killofki@yahoo.co.kr>
2019-01-07 21:15:37 +0900

    move to ..

--- up-and-down-js/script.js
+++ up-and-down-js/script.js
@@ -1,118 +1,140 @@
-let answer;
-let guess;
-let turn = 0;
-let gameStart = false;
-let postposition;
+{ 
+/// 
 
-const d = document;
-const queryBtn = d.getElementById('query'); 
-const msg = d.getElementById('msg');
-const atc = d.getElementsByTagName('article')[0];
-const inpt = d.getElementsByTagName('input')[0];
-const msgAbove = d.getElementById('message-above');
-const rst = d.getElementById('result');
-const msgBelow = d.getElementById('message-below');
+let answer; 
+let guess; 
+let turn = 0; 
+let gameStart = false; 
+let postposition; // ? 
 
-let initGameEventHandler = queryBtn.addEventListener("click", btnHandler);
+const queryBtn = $( '#query' ); 
+const msg = $( '#msg' ); 
+const atc = $( 'article'); 
+const inpt = $( 'input' ); 
+const msgAbove = $( '#message-above' ); 
+const rst = $( '#result' ); 
+const msgBelow = $( '#message-below' ); 
+const objects = { queryBtn, msg, atc, inpt, msgAbove, rst, msgBelow }; 
 
-function fadeIn(sth) {
-    sth.classList.add("active");
-    setTimeout(() => {sth.classList.add("show")}, 20);
-}
+function $( q ) { return document .querySelector( q ); } 
 
-function fadeOut(sth) {
-    sth.classList.remove("show");
-    setTimeout(() => {sth.classList.remove("active")}, 200);
-}
+let initGameEventHandler = queryBtn .addEventListener( "click", btnHandler ); 
 
-function btnHandler() {
-    if (gameStart) {
-        guess = parseInt(inpt.value);
-        
-        if ( isNaN(guess) || guess < 1 || guess > 100 ) {
-            msg.innerHTML = "1부터 100까지의 숫자 중 하나를 입력해주세요!";
-        } else {
-            inpt.value = "";
-            answering(guess);
-        }
+function fadeIn( sth ) { 
+	classDelay( sth, { add : 'active' } 
+		, [ 20, { add : 'show' } ] 
+		); 
+	} 
 
-    } else {
-        initializeGame();                                
-    }
-}
+function fadeOut( sth ) { 
+	classDelay( sth, { remove : 'show' } 
+		, [ 200, { remove : 'active' } ] 
+		); 
+	} 
 
-function initializeGame() {
-    fadeOut(atc);
-    turn = 0;
-    queryBtn.classList.add("deactivated");
-    queryBtn.innerHTML = "기다리세요!";
-    msg.innerHTML = "1부터 100까지의 숫자 중 하나를 생각하는 중입니다…";
-    
-    answer = Math.floor(Math.random() * 100) + 1;
-    setTimeout(startGame, 1000);
-}
+async function classDelay( obj, preCmd, ... delayCmds ) { 
+	classCmd( obj, preCmd ); 
+	for ( [ delay, timeoutCmd ] of delayCmds ) { 
+		await sleep( delay ); 
+		classCmd( obj, timeoutCmd ); 
+		} 
+	} 
 
-function startGame() {
-    queryBtn.classList.remove("deactivated");
-    queryBtn.innerHTML = "추측하기";
-    msg.innerHTML = "텍스트 창에 추측한 숫자를 입력하고 버튼을 눌려주세요.";
-    msgAbove.innerHTML = "1번째 시도입니다.";
-    msgBelow.innerHTML = "";
-    rst.innerHTML = "？";
-    
-    fadeIn(atc);
-    fadeIn(inpt);
-    gameStart = true;
+function sleep( delay ) { return new Promise( res => setTimeout( q => res(), delay ) ); } 
 
-    inpt.onkeydown = function(e) {
-        if (e.keyCode === 13) {
-            btnHandler();
-        }
-    };
-}
+function btnHandler() { 
+	  gameStart ? ( 
+		  guess = parseInt( inpt .value ) 
+		, ( isNaN( guess ) || guess < 1 || guess > 100 ) ? 
+			objectsNhtml( objects, { msg : "1부터 100까지의 숫자 중 하나를 입력해주세요!" } ) 
+		: ( inpt .value = "", answering( guess ) ) 
+		) 
+	: initializeGame() 
+		; 
+	} 
 
-function answering(num) {
-    if (num === "") {
-        return;
-    } else {
-        if (answer === num) {
-            result(true, num);
-        } else if (answer > num) {
-            result('up', num);
-        } else {
-            result('down', num);
-        }
-    }
-}
+function initializeGame() { 
+	fadeOut( atc ); 
+	turn = 0; 
+	classCmd( queryBtn, { add : 'deactivated' } ); 
+	objectsNhtml( objects, { 
+		  queryBtn : '기다리세요!' 
+		, msg : "1부터 100까지의 숫자 중 하나를 생각하는 중입니다…" 
+		} ); 
+	
+	answer = Math .floor( Math .random() * 100 ) + 1; 
+	setTimeout( startGame, 1000 ); 
+	} 
 
-function turnPass() {
-    turn++;
-    msgAbove.innerHTML = turn.toString() + "번째 시도입니다.";
-}
+function startGame() { 
+	classCmd( queryBtn, { remove : 'deactivated' } ); 
+	objectsNhtml( objects, { 
+		  queryBtn : '추측하기' 
+		, msg : "텍스트 창에 추측한 숫자를 입력하고 버튼을 눌려주세요." 
+		, msgAbove : "1번째 시도입니다." 
+		, msgBelow : "" 
+		, rst : "？" 
+		} ); 
+	
+	[ atc, inpt ] .forEach( fadeIn ); 
+	gameStart = true; 
+	
+	inpt .onkeydown = ({ keyCode }) => { keyCode === 13 && btnHandler(); }; 
+	} 
 
-function result(judge, num) {
-    if (judge === true) {
-        rst.className = "";
-        rst.innerHTML = answer;
-        msgBelow.innerHTML = "정답은 " + answer + "입니다!";
+function classCmd( obj, cmdobj ) { Object .keys( cmdobj ) .forEach( p => 
+	obj .classList[ p ]( cmdobj[ p ] ) 
+	); } 
 
-        gameStart = false;
-        queryBtn.innerHTML = "다시 하기";
-        msg.innerHTML = "게임을 다시 시작하려면 위의 버튼을 눌러주세요.";
-        fadeOut(inpt);
-    } else {
-        turnPass();
-        rst.className = "result-" + judge;
-        rst.innerHTML = judge.toUpperCase();
-        msgBelow.innerHTML = guess + choosePostposition(num) + " 아닙니다!"
-    }
-}
+function answering( num ) { 
+	  num === "" ? 'what?' // throw error..? 
+	: answer === num ? result( true, num ) 
+	: answer > num ? result( 'up', num ) 
+	: result( 'down', num ) 
+		; 
+	} 
 
-function choosePostposition(number) {
-    noun = parseInt(number) % 10;
-    if (noun === 2 || noun === 4 || noun === 5 || noun === 9) {
-        return "가";
-    } else {
-        return "이";
-    }
-}
+function turnPass() { 
+	turn += 1; 
+	objectsNhtml( objects, { msgAbove : `${ turn }번째 시도입니다.` } ); 
+	} 
+
+function result( judge, num ) { 
+	if ( judge === true ) { 
+		rst .className = ''; 
+		objectsNhtml( objects 
+			, { rst : answer } 
+			, { msgBelow : `정답은 ${ answer }입니다!` } 
+			); 
+		
+		gameStart = false; 
+		objectsNhtml( objects 
+			, { queryBtn : '다시하기' } 
+			, { msg : '게임을 다시 시작하려면 위의 버튼을 눌러주세요.' } 
+			); 
+		fadeOut( inpt ); 
+		} 
+	else { 
+		turnPass(); 
+		rst .className = `result-${ judge }`; 
+		objectsNhtml( objects 
+			, { rst : judge .toUpperCase() } 
+			, { msgBelow : `${ guess }${ choosePostposition( num ) } 아닙니다!` } 
+			);  
+		} 
+	} 
+
+function objectsNhtml( objs, ... orders ) { 
+	orders .forEach( cmdobj => Object .keys( cmdobj ) .forEach( p => 
+		objs[ p ] .innerHTML = cmdobj[ p ] 
+		) ); 
+	} 
+
+function choosePostposition( number ) { 
+	noun = parseInt( number ) % 10; 
+	return [ 2, 4, 5, 9 ] .some( n => n === noun ) ? '가' : '이'; 
+	} 
+
+// src from https://doro-nyong.github.io/up-and-down-js/ 
+/// 
+} 
